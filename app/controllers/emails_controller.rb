46348fe98addc98adf43292a128cebcf25ff8196
SaraswatiSaud <saraswati.saud@gmail.com>
2020-05-11 10:37:13 -0400

    Remove authentication

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,5 +1,5 @@
 class EmailsController < ApplicationController
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_email, only: [:show, :edit, :update, :destroy]
 
   # GET /emails
