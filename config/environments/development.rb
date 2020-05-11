181e6ea0f7e4401850c921b4a30d1a6705152fad
SaraswatiSaud <saraswati.saud@gmail.com>
2020-05-11 10:57:35 -0400

    Setup env variables

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,6 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.hosts << "0820a81a.ngrok.io"
+  config.hosts << "64798bcf.ngrok.io"
   config.action_mailbox.ingress = :sendgrid
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,8 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.ENV['RAILS_INBOUND_EMAIL_PASSWORD'] = :sendgrid
+  config.hosts << "64798bcf.ngrok.io"
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
